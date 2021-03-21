9963504f6896085204fa9ec0e97f076150f07827
Mike Dye <mike@mikedye.com>
2021-03-21 01:56:56 -0600

    data download and consolidation working

--- download_csv_date_range.py
+++ download_csv_date_range.py
@@ -1,23 +1,17 @@
 import pandas as pd
+
 import urllib.request
+from datetime import datetime
+
 
 # Define parameters for data retrieval
 INSTRUMENT_ID = '1'
 
+start_str = "2021-01-01"
+end_str = "2021-03-19"
 
+file_name = f'tzvolcano_data_instrument_id_{INSTRUMENT_ID}_{start_str}_to_{end_str}.csv'
 
-def load_olo_data(instrument_id, start, end):
-	# http://tzvolcano.chordsrt.com/api/v1/data/1.csv?start=2021-01-03T00:00&end=2021-01-04T00:00
-
-    url = f'http://tzvolcano.chordsrt.com/api/v1/data/{instrument_id}.csv?start={start}&end={end}'
-    print(url)
-
-    
-    return pd.read_csv(url,
-#                     index_col='Time', 
-                    parse_dates=['Time'],
-                    header=18
-                    )
 
 
 def download_olo_csv_file(instrument_id, start, end):
@@ -29,21 +23,12 @@ def download_olo_csv_file(instrument_id, start, end):
 	# print(html)
 	return(data)
 
-# original_data = load_olo_data(INSTRUMENT_ID, START, END)    
 
 
-import pandas as pd
-from datetime import datetime
 
-
-# pd.date_range(end = datetime.today(), periods = 100).to_pydatetime().tolist()
-
-#OR
-
-start_dates = pd.date_range(start="2021-01-01",end="2021-03-20")
+start_dates = pd.date_range(start=start_str, end=end_str)
 end_dates = start_dates + pd.DateOffset(days=1)
 
-file_name = "tzvolcano_concatneaded_data.csv"
 
 f = open(file_name, 'w')
 
@@ -63,14 +48,12 @@ for index, start_date in enumerate(start_dates):
 	# Write the header if this is the first download
 	if index == 0:
 	  header = "\n".join(lines[0:19])
-	  f.write(header)
-	  f.write("\n")
+	  f.write(header + "\n")
 
 	# write the rest of the data
-	f.write("\n".join(lines[20:len(lines)]))
+	f.write("\n".join(lines[20:len(lines)]) + "\n")
 
 	
-f.write("\n")
 f.close
 
 
