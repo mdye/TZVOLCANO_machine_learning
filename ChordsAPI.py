ec526fa2194a8ecd5cf140b62ce50f2b24ebd860
Mike Dye <mike@mikedye.com>
2021-03-27 00:47:12 -0600

    data download abstracted to python class

--- ChordsAPI.py
+++ ChordsAPI.py
@@ -1,21 +1,52 @@
 # Dependencies
 import urllib.request
-
+import pandas as pd
 
 class ChordsAPI:
-    def __init__(self):
-        self.domain				= 'tzvolcano.chordsrt.com'
+    def __init__(self, domain):
+        self.domain         = domain
+        self.base_api_dir   = 'api/v1/data/'
 
 
     def download_olo_csv_file(self, instrument_id, start, end):
-        domain				= 'tzvolcano.chordsrt.com'
-        base_api_dir = 'api/v1/data/'
-        url = f'http://{domain}/{base_api_dir}/{instrument_id}.csv?start={start}&end={end}'
+        url = f'http://{self.domain}/{self.base_api_dir}/{instrument_id}.csv?start={start}&end={end}'
+        
         print(url)
+
         with urllib.request.urlopen(url) as f:
             data = f.read().decode('utf-8')
 
         return(data)
 
-    # def get_csv_data(self, instrument_id, start, end):
-        
+
+    def get_csv_data(self, instrument_id, start_str, end_str):
+        start_dates = pd.date_range(start=start_str, end=end_str)
+        end_dates = start_dates + pd.DateOffset(days=1)
+
+        file_name = f'tzvolcano_data_instrument_id_{instrument_id}_{start_str}_to_{end_str}.csv'
+
+        f = open(file_name, 'w')
+
+
+        for index, start_date in enumerate(start_dates):
+            end_date = end_dates[index]
+
+            # FORMAT: 2021-01-03T00:00
+            start = start_date.strftime("%Y-%m-%dT00:00")
+            end = end_date.strftime("%Y-%m-%dT00:00")
+
+            print(index,start,end)
+
+            data_str = self.download_olo_csv_file(instrument_id, start, end)
+            lines = data_str.splitlines()
+
+            # Write the header if this is the first download
+            if index == 0:
+              header = "\n".join(lines[0:19])
+              f.write(header + "\n")
+
+            # write the rest of the data
+            f.write("\n".join(lines[20:len(lines)]) + "\n")
+
+            
+        f.close        
--- download_csv_date_range.py
+++ download_csv_date_range.py
@@ -1,12 +1,8 @@
-import pandas as pd
-
-import urllib.request
-from datetime import datetime
-
 
 from ChordsAPI import ChordsAPI
 
-chords_api =  ChordsAPI()
+domain = 'tzvolcano.chordsrt.com'
+chords_api =  ChordsAPI(domain)
 
 
 
@@ -14,53 +10,6 @@ chords_api =  ChordsAPI()
 INSTRUMENT_ID = '1'
 
 start_str = "2021-01-01"
-end_str = "2021-01-03"
+end_str = "2021-01-01"
 
-file_name = f'tzvolcano_data_instrument_id_{INSTRUMENT_ID}_{start_str}_to_{end_str}.csv'
-
-
-
-
-# def download_olo_csv_file(instrument_id, start, end):
-# 	url = f'http://tzvolcano.chordsrt.com/api/v1/data/{instrument_id}.csv?start={start}&end={end}'
-# 	print(url)
-# 	with urllib.request.urlopen(url) as f:
-# 		data = f.read().decode('utf-8')
-
-# 	# print(html)
-# 	return(data)
-
-
-
-
-start_dates = pd.date_range(start=start_str, end=end_str)
-end_dates = start_dates + pd.DateOffset(days=1)
-
-
-f = open(file_name, 'w')
-
-
-for index, start_date in enumerate(start_dates):
-	end_date = end_dates[index]
-
-	# FORMAT: 2021-01-03T00:00
-	start = start_date.strftime("%Y-%m-%dT00:00")
-	end = end_date.strftime("%Y-%m-%dT00:00")
-
-	print(index,start,end)
-
-	data_str = chords_api.download_olo_csv_file(INSTRUMENT_ID, start, end)
-	lines = data_str.splitlines()
-
-	# Write the header if this is the first download
-	if index == 0:
-	  header = "\n".join(lines[0:19])
-	  f.write(header + "\n")
-
-	# write the rest of the data
-	f.write("\n".join(lines[20:len(lines)]) + "\n")
-
-	
-f.close
-
-
+chords_api.get_csv_data(INSTRUMENT_ID, start_str, end_str)
