360ed330a392b525ec8d8eac4da2530f614774d8
Mike Dye <mike@mikedye.com>
2021-04-04 01:37:37 -0600

    GUI now can select data file

--- chords_gui.py
+++ chords_gui.py
@@ -9,6 +9,8 @@ from ipywidgets import interact, interactive, fixed, interact_manual, Layout
 import ipywidgets as widgets
 from datetime import datetime
 
+from ChordsAPI import ChordsAPI
+
 
 
 class chords_gui:
@@ -16,6 +18,14 @@ class chords_gui:
         self.start_datetime_default = datetime.fromisoformat('2020-02-01')
         self.end_datetime_default = datetime.fromisoformat('2020-02-01')
 
+        self.instrument_id = widgets.Select(
+            description='Instrument ID: ',
+            options=['1', '2', '3'],
+            value='1',
+            disabled=False,
+        )
+        
+        
         self.start_date = widgets.DatePicker(
             description='Start Date',
             value=self.start_datetime_default,
@@ -28,11 +38,70 @@ class chords_gui:
             disabled=False
         )
 
+        self.end_date = widgets.DatePicker(
+            description='End Date',
+            value=self.end_datetime_default,
+            disabled=False
+        )
+
+        
+        self.button = widgets.Button(
+            description='Download File',
+            disabled=False,
+#             button_style='', # 'success', 'info', 'warning', 'danger' or ''
+#             tooltip='Click me',
+#             icon='check' # (FontAwesome names without the `fa-` prefix)
+        )
+        
+        self.button.on_click(self.download_csv_file)
+        
+        self.available_data_files = widgets.Select(
+            options=self.get_availiable_files(),
+            description='',
+            disabled=False,
+            layout={'width': 'initial'}
+        )
+        
+        
+        self.out = widgets.Output()
+
         
     def start_end_widgets(self):
         
-        self.gui = widgets.HBox([self.start_date, self.end_date])
-
+        row_1 = widgets.HBox([self.instrument_id])
+        row_2 = widgets.HBox([self.start_date, self.end_date])
+        row_3 = widgets.HBox([self.button])
         
-        gui_output = widgets.Output()
-        display(self.gui, gui_output)
+#         gui_output = widgets.Output()
+
+        display(row_1, row_2, row_3, self.out)
+
+
+    def download_csv_file(self, passed_var):
+        instrument_id = self.instrument_id.value
+        start_str = self.start_date.value.strftime('%Y-%m-%d')
+        end_str = self.end_date.value.strftime('%Y-%m-%d')
+
+#         print(start_str)
+#         print(end_str)
+        
+        domain = 'tzvolcano.chordsrt.com'
+        chords_api =  ChordsAPI(domain)
+
+        chords_api.get_csv_data(instrument_id, start_str, end_str)
+        
+        
+    def get_availiable_files(self):
+        local_data_dir = 'csv_files'
+        from os import listdir
+        from os.path import isfile, join
+        files = [f for f in listdir(local_data_dir) if isfile(join(local_data_dir, f))]
+
+        return(files)
+    
+
+    def select_data_file(self):        
+        print("Available Data Files")
+        data_files = widgets.HBox([self.available_data_files])
+
+        display(data_files, self.out)
