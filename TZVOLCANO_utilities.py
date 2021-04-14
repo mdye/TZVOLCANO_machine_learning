7e71c415141c0f813a1d231d2d26206b20195540
Mike Dye <mike@mikedye.com>
2021-04-14 00:10:28 -0600

    refactor working, still needs cleanup

--- TZVOLCANO_utilities.py
+++ TZVOLCANO_utilities.py
@@ -9,6 +9,8 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+from matplotlib.colors import LogNorm
+
 
 # TensorFlow ≥2.0 is required
 import tensorflow as tf
@@ -16,9 +18,9 @@ from tensorflow import keras
 assert tf.__version__ >= "2.0"
 
 
-def plot_series(series, n_steps, y=None, y_pred=None, x_label="$t$", y_label="$x(t)$"):
-    import matplotlib.pyplot as plt
 
+
+def plot_series(series, n_steps, y=None, y_pred=None, x_label="$t$", y_label="$x(t)$"):
     plt.plot(series, ".-")
     if y is not None:
         plt.plot(n_steps, y, "bx", markersize=10)
@@ -35,8 +37,6 @@ def plot_series(series, n_steps, y=None, y_pred=None, x_label="$t$", y_label="$x
     
     
 def plot_multiple_forecasts(X, Y, Y_pred, n_steps):
-    import matplotlib.pyplot as plt
-
     n_steps = X.shape[1]
     ahead = Y.shape[1]
     plot_series(X[0, :, 0], n_steps)
@@ -44,6 +44,12 @@ def plot_multiple_forecasts(X, Y, Y_pred, n_steps):
     plt.plot(np.arange(n_steps, n_steps + ahead), Y_pred[0, :, 0], "bx-", label="Forecast", markersize=10)
     plt.axis([0, n_steps + ahead, -1, 1])
     plt.legend(fontsize=14)    
+    
+    
+def plot_clusters(X, y=None):
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=1)
+    plt.xlabel("$x_1$", fontsize=14)
+    plt.ylabel("$x_2$", fontsize=14, rotation=0)    
     
     
 def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
@@ -63,3 +69,90 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
     
 def last_time_step_mse(Y_true, Y_pred):
     return keras.metrics.mean_squared_error(Y_true[:, -1], Y_pred[:, -1])
+
+
+def scale_np_data(np_array, scale_minimum, scale_maximum):
+    scaled_data_np = np.interp(np_array, (np_array.min(), np_array.max()), (scale_minimum, scale_maximum))
+
+    return scaled_data_np
+
+
+def get_RNN_training_sets(pandas_object, key, n_steps):
+    np_data = pandas_object[key].to_numpy()
+
+#     height_forecast = np_data[n_steps_training-n_steps_forecast:n_steps_training + n_steps_ahead]
+
+    
+    np_data = np_data[:n_steps+1]
+
+    np_series = np.array([np_data])
+    np_series = np_series[..., np.newaxis].astype(np.float32)
+
+    np_training_data = np.array(np_series[:,:-1])
+    np_labels = np.array(np_series[:,-1])    
+    
+    return np_training_data, np_labels
+
+
+def get_RNN_forecast_sets(pandas_object, key, n_steps_training, n_steps_forecast, n_steps_ahead):
+    
+    np_data = pandas_object[key].to_numpy()
+
+    # Get the tailing records in the data set 
+#     np_data = np_data[n_steps_training - n_steps_forecast: n_steps_training + n_steps_ahead]
+    np_data = np_data[-(n_steps_forecast + n_steps_ahead):]
+
+    forecast_series = np.array([np_data])
+
+    # Cast the data so they are float32
+    forecast_series = forecast_series[..., np.newaxis].astype(np.float32)
+    
+    np_training_data = forecast_series[:, :n_steps_forecast]
+    np_labels = forecast_series[:, n_steps_forecast:]
+    
+    return np_training_data, np_labels
+
+
+
+
+
+def plot_centroids(centroids, weights=None, circle_color='w', cross_color='k'):
+    if weights is not None:
+        centroids = centroids[weights > weights.max() / 10]
+    plt.scatter(centroids[:, 0], centroids[:, 1],
+                marker='o', s=35, linewidths=8,
+                color=circle_color, zorder=10, alpha=0.9)
+    plt.scatter(centroids[:, 0], centroids[:, 1],
+                marker='x', s=2, linewidths=12,
+                color=cross_color, zorder=11, alpha=1)
+    
+
+def plot_gaussian_mixture(clusterer, X, resolution=1000, show_ylabels=True):
+    mins = X.min(axis=0) - 0.1
+    maxs = X.max(axis=0) + 0.1
+    xx, yy = np.meshgrid(np.linspace(mins[0], maxs[0], resolution),
+                         np.linspace(mins[1], maxs[1], resolution))
+    Z = -clusterer.score_samples(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    plt.contourf(xx, yy, Z,
+                 norm=LogNorm(vmin=1.0, vmax=30.0),
+                 levels=np.logspace(0, 6, 96))
+    plt.contour(xx, yy, Z,
+                norm=LogNorm(vmin=1.0, vmax=30.0),
+                levels=np.logspace(0, 6, 96),
+                linewidths=1, colors='k')
+
+    Z = clusterer.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.contour(xx, yy, Z,
+                linewidths=2, colors='r', linestyles='dashed')
+    
+    plt.plot(X[:, 0], X[:, 1], 'k.', markersize=2)
+    plot_centroids(clusterer.means_, clusterer.weights_)
+
+    plt.xlabel("$x_1$", fontsize=14)
+    if show_ylabels:
+        plt.ylabel("$x_2$", fontsize=14, rotation=0)
+    else:
+        plt.tick_params(labelleft=False)    
