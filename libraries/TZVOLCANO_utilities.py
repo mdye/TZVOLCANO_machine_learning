8d7c1f6c0f645500944a6fe7919a9ec444915697
Mike Dye <mike@mikedye.com>
2021-05-05 02:48:35 -0600

    Code abstracted to get_anomalies_using_gaussian_mixtures function

--- libraries/TZVOLCANO_utilities.py
+++ libraries/TZVOLCANO_utilities.py
@@ -25,6 +25,10 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 
+# Machine Learning Algorithms
+from sklearn.mixture import GaussianMixture
+
+
 
 
 def calculate_vector_magnitude(pandas_object, vector_fields_list, scale_minimum, scale_maximum):
@@ -281,6 +285,27 @@ def transform_data_for_gaussian_mixtures(pandas_object, field_name):
     return data_imputed
 
 
+
+def get_anomalies_using_gaussian_mixtures(data_imputed, density_threshold_percent):
+    # Gaussian Mixtures Parameters
+    N_COMPONENTS = 1           # The number of regions to generate - needs to be 1 for this use case
+    N_INIT = 10
+    COVARIANCE_TYPE = "tied"
+    
+    gm = GaussianMixture(n_components=N_COMPONENTS, n_init=N_INIT,covariance_type=COVARIANCE_TYPE, random_state=42)
+    gm.fit(data_imputed)
+
+    # gm.predict(vector_magnitude_data_imputed)
+    # gm.predict_proba(vector_magnitude_data_imputed)
+    # gm.score_samples(vector_magnitude_data_imputed)
+
+    densities = gm.score_samples(data_imputed)
+
+    density_threshold = np.percentile(densities, density_threshold_percent)
+
+    anomalies = data_imputed[densities < density_threshold]   
+    
+    return gm, anomalies
 
 def get_rnn_model(n_steps_ahead):
     np.random.seed(42)
