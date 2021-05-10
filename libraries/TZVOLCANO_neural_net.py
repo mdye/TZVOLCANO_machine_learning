8590cd7b32ca092392856ae7820e011e4ff8159d
Mike Dye <mike@mikedye.com>
2021-05-10 02:08:26 -0600

    neural net library now well commented

--- libraries/TZVOLCANO_neural_net.py
+++ libraries/TZVOLCANO_neural_net.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-
 # TensorFlow ≥2.0 is required
 import tensorflow as tf
 from tensorflow import keras
 assert tf.__version__ >= "2.0"
+
 
 
 #####
@@ -13,8 +13,6 @@ assert tf.__version__ >= "2.0"
 #####
 def last_time_step_mse(Y_true, Y_pred):
     return keras.metrics.mean_squared_error(Y_true[:, -1], Y_pred[:, -1])
-
-
 
 
 #####
@@ -69,9 +67,9 @@ def get_RNN_forecast_sets(pandas_object, key, n_steps_training, n_steps_forecast
     return np_training_data, np_labels
 
 
-
 #####
 # return a keras object containing the neural network model to be used 
+#
 # Note: In development of the notebook and support libraries, a number of different neural new models were investigated.
 #       While only the final model used in the notebook is actually used, the configuration for the other models are 
 #       retained in case the are of use in future development
@@ -87,7 +85,16 @@ def get_neural_net_model(n_steps_ahead):
 
 
 
-def get_1D_convolutional_and_gru_with_dropout_model(n_steps_ahead):
+#####
+# This keras model defines a neural net with several layers:
+# Top     : A 1D convolutional to assist in dimensionality reduction of the data
+# Top - 1 : A GRU layer (Gated Recurrent Unit) which acts similar to a recurrent neural net but is not as susecptible to 
+#          disappearing/expoding gradients
+# Top - 2 : A dropout layer to reduce this model's tendency to overfit with limited input data
+# Top - 3 : Another GRU layer
+# Top - 4 : A time distibuted dense layer set to output an *n_steps_ahead* number of values
+#####
+def get_time_distributed_1D_convolutional_and_gru_with_dropout_model(n_steps_ahead):
 
     model = keras.models.Sequential([
         keras.layers.Conv1D(filters=20, kernel_size=4, strides=2, padding="valid",
@@ -98,88 +105,111 @@ def get_1D_convolutional_and_gru_with_dropout_model(n_steps_ahead):
         keras.layers.TimeDistributed(keras.layers.Dense(n_steps_ahead))
     ])
 
-#     model.compile(loss="mse", optimizer="adam", metrics=[last_time_step_mse])    
     model.compile(loss="mse", optimizer=keras.optimizers.Adam(learning_rate=0.01), metrics=[last_time_step_mse])
     
     return model
 
 
-def get__model(n_steps_ahead):
+
+
+
+
+
+#####
+# Below are other neural net models that were investigated during development
+#####
+
+def get_time_distributed_rnn_with_batch_normalization_model(n_steps_ahead):
+
+    model = keras.models.Sequential([
+        keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+        keras.layers.BatchNormalization(),
+        keras.layers.SimpleRNN(20, return_sequences=True),
+        keras.layers.BatchNormalization(),
+        keras.layers.TimeDistributed(keras.layers.Dense(n_steps_ahead))
+    ])    
+    
+    model.compile(loss="mse", optimizer="adam", metrics=[last_time_step_mse])
+        
+    return model
+
+
+def get_time_distributed_rnn_with_mse_loss_and_custom_learning_rate_model(n_steps_ahead):
+    # This model makes fairly good predictions
+    
+    model = keras.models.Sequential([
+        keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+        keras.layers.SimpleRNN(20, return_sequences=True),
+        keras.layers.TimeDistributed(keras.layers.Dense(n_steps_ahead))
+    ])
+
+    model.compile(loss="mse", optimizer=keras.optimizers.Adam(learning_rate=0.01), metrics=[last_time_step_mse])
+
+    return model
+
+    
+def get_rnn_model(n_steps_ahead):
+
+    model = keras.models.Sequential([
+        keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+        keras.layers.SimpleRNN(20),
+        keras.layers.Dense(n_steps_ahead)
+    ])
+    
+    model.compile(loss="mse", optimizer="adam")
     
     return model
 
 
-#     model = keras.models.Sequential([
-#         keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
-#         keras.layers.BatchNormalization(),
-#         keras.layers.SimpleRNN(20, return_sequences=True),
-#         keras.layers.BatchNormalization(),
-#         keras.layers.TimeDistributed(keras.layers.Dense(n_steps_ahead))
-#     ])    
+def get_dense_rnn_with_mse_loss_and_custom_learning_rate_model(n_steps_ahead):
+
+    model = keras.models.Sequential([
+        keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+        keras.layers.SimpleRNN(20),
+        keras.layers.Dense(n_steps_ahead)
+    ])
     
-#     model.compile(loss="mse", optimizer="adam", metrics=[last_time_step_mse])
+    model.compile(loss="mse", optimizer=keras.optimizers.Adam(learning_rate=0.01), metrics=[last_time_step_mse])
+    
+    return model
 
 
+def get_time_distributed_rnn_with_dropout_and_custom_learning_rate_model(n_steps_ahead):
+
+    model = keras.models.Sequential([
+        keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+        keras.layers.Dropout(rate=0.2),
+        keras.layers.SimpleRNN(20, return_sequences=True),
+        keras.layers.TimeDistributed(keras.layers.Dense(n_steps_ahead))
+    ])
+
+    model.compile(loss="mse", optimizer=keras.optimizers.Adam(learning_rate=0.01), metrics=[last_time_step_mse])
+    
+    return model
 
 
+def get_time_distributed_LSTM_with_custom_learning_rate_model(n_steps_ahead):
+    model = keras.models.Sequential([
+        keras.layers.LSTM(20, return_sequences=True, input_shape=[None, 1]),
+        keras.layers.LSTM(20, return_sequences=True),
+        keras.layers.TimeDistributed(keras.layers.Dense(n_steps_ahead))
+    ])
 
-# This is the model that produces decent results
-#     model = keras.models.Sequential([
-#         keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
-#         keras.layers.SimpleRNN(20, return_sequences=True),
-#         keras.layers.TimeDistributed(keras.layers.Dense(n_steps_ahead))
-#     ])
-
-#     model.compile(loss="mse", optimizer=keras.optimizers.Adam(learning_rate=0.01), metrics=[last_time_step_mse])
+    model.compile(loss="mse", optimizer=keras.optimizers.Adam(learning_rate=0.01), metrics=[last_time_step_mse])
+    
+    return model
     
 
+def get_time_distributed_LSTM_mse_loss_model(n_steps_ahead):
+    model = keras.models.Sequential([
+        keras.layers.LSTM(20, return_sequences=True, input_shape=[None, 1]),
+        keras.layers.LSTM(20, return_sequences=True),
+        keras.layers.TimeDistributed(keras.layers.Dense(n_steps_ahead))
+    ])
+
+    model.compile(loss="mse", optimizer="adam", metrics=[last_time_step_mse])
     
+    return model
 
-#     model = keras.models.Sequential([
-#         keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
-#         keras.layers.SimpleRNN(20),
-#         keras.layers.Dense(n_steps_ahead)
-#     ])
-    
-#     model.compile(loss="mse", optimizer="adam")
-
-
-#     model = keras.models.Sequential([
-#         keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
-#         keras.layers.SimpleRNN(20),
-#         keras.layers.Dense(n_steps_ahead)
-#     ])
-    
-#     model.compile(loss="mse", optimizer=keras.optimizers.Adam(learning_rate=0.01), metrics=[last_time_step_mse])
-
-
-
-    
-#     model = keras.models.Sequential([
-#         keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
-#         keras.layers.Dropout(rate=0.2),
-#         keras.layers.SimpleRNN(20, return_sequences=True),
-#         keras.layers.TimeDistributed(keras.layers.Dense(n_steps_ahead))
-#     ])
-
-#     model.compile(loss="mse", optimizer=keras.optimizers.Adam(learning_rate=0.01), metrics=[last_time_step_mse])
-    
-    
-#     model = keras.models.Sequential([
-#         keras.layers.LSTM(20, return_sequences=True, input_shape=[None, 1]),
-#         keras.layers.LSTM(20, return_sequences=True),
-#         keras.layers.TimeDistributed(keras.layers.Dense(n_steps_ahead))
-#     ])
-
-#     model.compile(loss="mse", optimizer=keras.optimizers.Adam(learning_rate=0.01), metrics=[last_time_step_mse])
-
-    
-#     model = keras.models.Sequential([
-#         keras.layers.LSTM(20, return_sequences=True, input_shape=[None, 1]),
-#         keras.layers.LSTM(20, return_sequences=True),
-#         keras.layers.TimeDistributed(keras.layers.Dense(n_steps_ahead))
-#     ])
-
-#     model.compile(loss="mse", optimizer="adam", metrics=[last_time_step_mse])
 
 
